4039b0a72a1cd95874c278de6f2914d3a95c8988
LightningGod7 <lightninggod7@users.noreply.github.com>
2026-08-08 14:07:56 -0500

    security: rate-limit brute force and use CSPRNG session identifiers

--- vulnerabilities/weak_id/source/high.php
+++ vulnerabilities/weak_id/source/high.php
@@ -3,12 +3,9 @@
 $html = "";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-	if (!isset ($_SESSION['last_session_id_high'])) {
-		$_SESSION['last_session_id_high'] = 0;
-	}
-	$_SESSION['last_session_id_high']++;
-	$cookie_value = md5($_SESSION['last_session_id_high']);
-	setcookie("dvwaSession", $cookie_value, time()+3600, "/vulnerabilities/weak_id/", $_SERVER['HTTP_HOST'], false, false);
+	// Session identifiers must be unpredictable: 160 bits from a CSPRNG.
+	// The cookie is additionally scoped and flagged HttpOnly and Secure.
+	$cookie_value = bin2hex(random_bytes(20));
+	setcookie("dvwaSession", $cookie_value, time()+3600, "/vulnerabilities/weak_id/", $_SERVER['HTTP_HOST'], true, true);
 }
-
 ?>
--- vulnerabilities/weak_id/source/medium.php
+++ vulnerabilities/weak_id/source/medium.php
@@ -3,7 +3,9 @@
 $html = "";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-	$cookie_value = time();
-	setcookie("dvwaSession", $cookie_value);
+	// Session identifiers must be unpredictable: 160 bits from a CSPRNG.
+	// The cookie is additionally scoped and flagged HttpOnly and Secure.
+	$cookie_value = bin2hex(random_bytes(20));
+	setcookie("dvwaSession", $cookie_value, time()+3600, "/vulnerabilities/weak_id/", $_SERVER['HTTP_HOST'], true, true);
 }
 ?>
